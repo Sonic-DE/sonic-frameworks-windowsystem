7fcfe125b560aca22f4b6b8e8e84b88522b76182
Nicolas Fella <nicolas.fella@gmx.de>
2023-02-02 17:44:07 +0100

    Extract Wayland-specific bits of KWindowSystem into KWaylandExtras
    KWindowSystem has some Wayland-specific code for using XDG activation that can't easily be abstracted to cover X11 too
    
    Extract that into a KWaylandExtras class, similar to how we extracted X11-specific code into KX11Extras

---
+++ src/kwaylandextras_interface.cpp
@@ -0,0 +1,16 @@
+/*
+    SPDX-FileCopyrightText: 2023 Nicolas Fella <nicolas.fella@gmx.de>
+
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
+#include "kwaylandextras_interface_p.h"
+
+KWaylandExtrasPluginInterface::KWaylandExtrasPluginInterface(QObject *parent)
+    : QObject(parent)
+{
+}
+
+KWaylandExtrasPluginInterface::~KWaylandExtrasPluginInterface()
+{
+}
