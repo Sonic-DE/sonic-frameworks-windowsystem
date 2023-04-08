b6de04132e16768fd31cc3ab08484283720630fa
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-04-08 09:09:04 +0200

    kwindowsystemplugin: Set ownership for QML singletons

--- src/qml/kwindowsystemplugin.cpp
+++ src/qml/kwindowsystemplugin.cpp
@@ -21,12 +21,13 @@ void KWindowSystemPlugin::registerTypes(const char *uri)
     Q_ASSERT(QString::fromLatin1(uri) == QLatin1String("org.kde.kwindowsystem"));
 
 #if KWINDOWSYSTEM_HAVE_X11
-    qmlRegisterSingletonType<KX11Extras>("org.kde.kwindowsystem", 1, 0, "KX11Extras", [](QQmlEngine *, QJSEngine *) -> QObject * {
+    qmlRegisterSingletonType<KX11Extras>("org.kde.kwindowsystem", 1, 0, "KX11Extras", [](QQmlEngine *engine, QJSEngine *) -> QObject * {
+        engine->setObjectOwnership(KX11Extras::self(), QQmlEngine::CppOwnership);
         return KX11Extras::self();
     });
 #endif
-
-    qmlRegisterSingletonType<KWindowSystem>("org.kde.kwindowsystem", 1, 0, "KWindowSystem", [](QQmlEngine *, QJSEngine *) -> QObject * {
+    qmlRegisterSingletonType<KWindowSystem>("org.kde.kwindowsystem", 1, 0, "KWindowSystem", [](QQmlEngine *engine, QJSEngine *) -> QObject * {
+        engine->setObjectOwnership(KWindowSystem::self(), QQmlEngine::CppOwnership);
         return KWindowSystem::self();
     });
 }
