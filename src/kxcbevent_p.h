fc24181acccfd1280483106c7780197fb2a07659
Andreas Hartmetz <ahartmetz@gmail.com>
2024-04-01 22:17:13 +0200

    Introduce KXcbEvent to initialize the memory of sent XCB events
    The XCB events were often stack-allocated and partially
    uninitialized - valgrind complained in some cases. The XCB
    documentation recommends using calloc(32, 1) for them, but we can
    also use the stack and initialize the memory using a little
    wrapper. Also make sure that the initialized memory has the right
    *size*, because according to XCB docs, 32 bytes are always sent,
    so 32 bytes should be initialized to prevent leaking information.

---
+++ src/kxcbevent_p.h
@@ -0,0 +1,49 @@
+// KXcbEvent allocates the xcb event in a 32 bytes large, zero-initialized buffer to avoid
+// out-of-bounds reads and reading data from the stack. Also add alignment for good measure.
+// According to XCB documentation, the wire size of all XCB events is 32 bytes, so this should
+// work to initialize all the data that will be sent.
+
+#include <tuple>
+#include <type_traits>
+
+template<class T, bool needsPadding = (sizeof(T) < 32)>
+struct KXcbEvent;
+
+template<class T>
+class alignas(size_t) KXcbEvent<T, false> : public T
+{
+public:
+    inline KXcbEvent()
+    {
+        static_assert(sizeof(*this) == evt_size);
+        memset(this, 0, evt_size);
+    }
+
+    const char *buffer() const
+    {
+        return reinterpret_cast<const char *>(this);
+    }
+
+private:
+    static constexpr size_t evt_size = 32;
+};
+
+template<class T>
+class alignas(size_t) KXcbEvent<T, true> : public T
+{
+public:
+    inline KXcbEvent()
+    {
+        static_assert(sizeof(*this) == evt_size);
+        memset(this, 0, evt_size);
+    }
+
+    const char *buffer() const
+    {
+        return reinterpret_cast<const char *>(this);
+    }
+
+private:
+    static constexpr size_t evt_size = 32;
+    char filler[evt_size - sizeof(T)];
+};
