f59f761304e5bd77418a3a81201009331a4bf8b9
Nicolas Fella <nicolas.fella@gmx.de>
2024-04-02 20:57:49 +0200

    Port QML module to declarative type registration

---
+++ src/qml/types.h
@@ -0,0 +1,43 @@
+/*
+    SPDX-FileCopyrightText: 2024 Nicolas Fella <nicolas.fella@gmx.de>
+
+    SPDX-License-Identifier: LGPL-2.1-or-later
+*/
+
+#ifndef KWINDOWSYSTEM_QML_TYPES_H
+#define KWINDOWSYSTEM_QML_TYPES_H
+
+#include <QQmlEngine>
+
+#include <KWindowSystem>
+#include <KX11Extras>
+
+struct KWindowSystemForeign {
+    Q_GADGET
+    QML_NAMED_ELEMENT(KWindowSystem)
+    QML_SINGLETON
+    QML_FOREIGN(KWindowSystem)
+
+public:
+    static KWindowSystem *create(QQmlEngine *, QJSEngine *)
+    {
+        QQmlEngine::setObjectOwnership(KWindowSystem::self(), QQmlEngine::CppOwnership);
+        return KWindowSystem::self();
+    }
+};
+
+struct KX11ExtrasForeign {
+    Q_GADGET
+    QML_NAMED_ELEMENT(KX11Extras)
+    QML_SINGLETON
+    QML_FOREIGN(KX11Extras)
+
+public:
+    static KX11Extras *create(QQmlEngine *, QJSEngine *)
+    {
+        QQmlEngine::setObjectOwnership(KX11Extras::self(), QQmlEngine::CppOwnership);
+        return KX11Extras::self();
+    }
+};
+
+#endif
